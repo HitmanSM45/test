b0d85ae11dbd04c9ef7a6400df861eb0dfabbfb9
Shubham <shubhammohite33@gmail.com>
2021-04-11 17:19:28 +0530

    [shubham] sending sample1 ....

--- src/sm123/sample1.java
+++ src/sm123/sample1.java
@@ -3,6 +3,9 @@ package sm123;
 public class sample1 {
 	public static void main(String[] args) {
 		System.out.println("hi this is shubham");
+		System.out.println("hi Navtej");
+		System.out.println("hi Prathamesh");
+		System.out.println("hello Aniket");
 	}
 
 }
