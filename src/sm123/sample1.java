df8db793574c7415152115487178183d3a027a6b
HP <HP@192.168.1.206>
2021-04-12 14:26:27 +0530

    [Nishant] updated code

--- src/sm123/sample1.java
+++ src/sm123/sample1.java
@@ -3,6 +3,7 @@ package sm123;
 public class sample1 {
 	public static void main(String[] args) {
 		System.out.println("hi this is shubham");
+		System.out.println("hii shubhamdada from Nishant");
 	}
 
 }
